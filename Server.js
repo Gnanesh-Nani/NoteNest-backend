e4f5c30b7df312f47af8c19f57ff35dbe7169c38
Balajicg5 <balaji989412@gmail.com>
2025-04-26 23:10:25 +0530

    route change

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors=require('cors');
 const connectDB = require('./config/db');
 
 // Controllers and routes
@@ -7,7 +8,10 @@ const authRoutes = require('./api/auth');
 const noteRoutes = require('./api/notes');
 
 const app = express();
-
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+// Enable CORS for all routes
 // Middleware to parse JSON
 app.use(express.json());
 
@@ -19,9 +23,6 @@ connectDB();
 app.use('/api/auth', authRoutes);  // Authentication routes
 app.use('/api/notes', noteRoutes); // Notes routes
 
-app.use('/', (req, res) => {
-  res.send('Welcome to NoteNest API!');
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
