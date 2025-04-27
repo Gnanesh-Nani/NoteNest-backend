9d29af4572935132cf273ab5b95ec7aa3abcc4a0
Gnanesh-Nani <gnaneshmassboy@gmail.com>
2025-04-27 19:01:54 +0530

    added cors and 404 path

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
-
+const cors = require('cors');
 // Controllers and routes
 const authRoutes = require('./api/auth');
 const noteRoutes = require('./api/notes');
@@ -11,6 +11,12 @@ const app = express();
 // Middleware to parse JSON
 app.use(express.json());
 
+app.use(cors({
+  origin: process.env.CROSS_ORIGIN || '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+  credentials: true,
+}));
+
 // Connect to MongoDB
 connectDB();
 
@@ -19,9 +25,14 @@ connectDB();
 app.use('/api/auth', authRoutes);  // Authentication routes
 app.use('/api/notes', noteRoutes); // Notes routes
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Welcome to NoteNest API!');
 });
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+}
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
