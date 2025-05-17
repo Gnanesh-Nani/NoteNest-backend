3a2c306ca1e81fb0874ebc492cd0ad20ca271f6c
Balajicg5 <balaji989412@gmail.com>
2025-05-17 16:29:47 +0530

    resolved error

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors=require('cors');
 const connectDB = require('./config/db');
-const cors = require('cors');
+
 // Controllers and routes
 const authRoutes = require('./api/auth');
 const noteRoutes = require('./api/notes');
